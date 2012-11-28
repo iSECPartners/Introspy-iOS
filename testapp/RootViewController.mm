8166a7fa492a4698e17ef0cfbb306253dad06400
Alban D <nabla.c0d3@gmail.com>
2012-11-28 11:39:52 -0800

    Initial commit for the test App

---
+++ testapp/RootViewController.mm
@@ -0,0 +1,8 @@
+#import "RootViewController.h"
+
+@implementation RootViewController
+- (void)loadView {
+	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
+	self.view.backgroundColor = [UIColor redColor];
+}
+@end
