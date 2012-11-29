476c6ff805e4d2bb757cf3744cabc60c85f2bd1b
Alban D <nabla.c0d3@gmail.com>
2012-11-28 19:00:44 -0800

    Add first test

--- testapp/RootViewController.mm
+++ testapp/RootViewController.mm
@@ -4,5 +4,34 @@
 - (void)loadView {
 	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
 	self.view.backgroundColor = [UIColor redColor];
+
+
+
+    UIAlertView *alert =
+        [[UIAlertView alloc] initWithTitle: @"Introspy Test App"
+                             message: @"Introspy Test App Started"
+                             delegate: self
+                             cancelButtonTitle: @"OK"
+                             otherButtonTitles: nil];
+    [alert show];
+    [alert release];
+
+    // Data Storage
+
+    //NSFileManager
+    NSLog(@"**************Test**************");
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+
+    //createFileAtPath
+    NSString* path = [NSHomeDirectory() stringByAppendingPathComponent:@"test.file"];
+    //NSDictionary attributes;
+    NSString* myString = @"testing12345";
+    NSData* contents = [myString dataUsingEncoding: [NSString defaultCStringEncoding]];
+
+   	[fileManager createFileAtPath:path contents:contents attributes:nil];
+
+
+   	// End
+
 }
 @end
