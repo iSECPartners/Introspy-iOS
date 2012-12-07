85678df9fd418d05eeec2fb0e6bc7d85e9fc7818
Alban D <nabla.c0d3@gmail.com>
2012-12-06 17:01:50 -0800

    NSUserDefaults tests

--- testapp/DataStorageTester.m
+++ testapp/DataStorageTester.m
@@ -28,11 +28,11 @@ static NSData *testContent;
 
 
 - (void)runTests {
-    
-    NSLog(@"**************Test**************");
+
     [self testKeyChain];
     [self testNSFileManager];
     [self testNSData];
+    [self testNSUserDefaults];
 }
 
 
@@ -85,16 +85,46 @@ static NSData *testContent;
 
 
 - (void)testNSUserDefaults {
-    // TBD
-    /*
-    (NSUserDefaults *) defaults = [NSUserDefaults standardUserDefaults];
 
-    [defaults setObject:firstName forKey:@"firstName"];
-    [defaults setObject:lastName forKey:@"lastname"];
-    [defaults setInteger:age forKey:@"age"];
-    [defaults setObject:imageData forKey:@"image"];
-    [defaults synchronize];
-   */
+    NSMutableString *testKey = [NSMutableString stringWithCapacity:15];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [testKey setString:@"testBool"];
+    [defaults setBool:YES forKey:testKey];
+    [defaults boolForKey:testKey];
+
+    [testKey setString:@"testFloat"];
+    [defaults setFloat:5.5 forKey:testKey];
+    [defaults floatForKey:testKey];
+
+    [testKey setString:@"testInt"];
+    [defaults setInteger:1 forKey:testKey];
+    [defaults integerForKey:testKey];
+
+    [testKey setString:@"testDouble"];
+    [defaults setDouble:1 forKey:testKey];
+    [defaults doubleForKey:testKey];
+
+    [testKey setString:@"testObject"];
+    [defaults setObject:@"testString" forKey:testKey];
+    [defaults objectForKey:testKey];
+    [defaults stringForKey:testKey];
+
+    NSArray *testArray;
+    [testKey setString:@"testArray"];
+    testArray = [NSArray arrayWithObjects:@"test1", @"test2", nil];
+    [defaults setObject:testArray forKey:testKey];
+    [defaults stringArrayForKey:testKey];
+    [defaults arrayForKey:testKey];
+    [defaults dictionaryForKey:testKey];
+    
+    [testKey setString:@"testData"];
+    [defaults setObject:testContent forKey:testKey];
+    [defaults dataForKey:testKey];
+
+    [testKey setString:@"testURL"];
+    [defaults setURL:testURL forKey:testKey];
+    [defaults URLForKey:testKey];    
 }
 
 
