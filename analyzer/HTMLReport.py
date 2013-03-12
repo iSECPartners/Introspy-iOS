4fea44694af60179464001517b4789372995515d
Alban D <nabla.c0d3@gmail.com>
2013-03-12 15:10:21 -0700

    Copy full template in one call

--- analyzer/HTMLReport.py
+++ analyzer/HTMLReport.py
@@ -1,4 +1,4 @@
-import os, shutil
+import shutil
 from TraceStorage import TraceStorage
 
 class HTMLReport:
@@ -9,13 +9,8 @@ class HTMLReport:
         
     def write_to_directory(self, directory):
         
-        # Create the directory
-        os.makedirs(directory)
-        
         # Copy the template
-        shutil.copy('./html/report.html', directory)
-        shutil.copy('./html/jquery.min.js', directory)
-        shutil.copy('./html/handlebars.js', directory)
+        shutil.copytree('./html', directory)
         
         # Dump the traced calls
         self.traceStorage.dump_to_JS(directory)
