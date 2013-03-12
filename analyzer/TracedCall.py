565f3371a3f4c73fcc590e70e9032a696774a247
Alban D <nabla.c0d3@gmail.com>
2013-03-12 14:57:33 -0700

    No max line length for base64 encoding

--- analyzer/TracedCall.py
+++ analyzer/TracedCall.py
@@ -53,7 +53,7 @@ class TracedCallJSONEncoder(json.JSONEncoder):
 			try: # Does it seem to be ASCII ?
 				data = obj.data.encode('ascii')
 			except UnicodeDecodeError: # No => base64 encode it
-				data = obj.asBase64()
+				data = obj.asBase64(maxlinelength=1000000)
 			return data
 		else:
 			return super(TracedCallJSONEncoder, self).default(obj)
