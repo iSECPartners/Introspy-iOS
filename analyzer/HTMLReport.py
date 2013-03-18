4503c7721c2114fcdd95619c6cd3a37b09699dd9
Alban D <nabla.c0d3@gmail.com>
2013-03-17 18:57:59 -0700

    Save the original DB file

--- analyzer/HTMLReport.py
+++ analyzer/HTMLReport.py
@@ -8,15 +8,19 @@ class HTMLReport:
     Generates an HTML report given an Introspy DB file and 
     a list of vuln signatures to check for.
     """
-    def __init__(self, introspy_db_path, signature_list):
-        self.traceStorage = TraceStorage(introspy_db_path)
-        self.analyzer = Analyzer(introspy_db_path, signature_list)
+    def __init__(self, introspy_db_filename, signature_list):
+        self.db_filename = introspy_db_filename
+        self.traceStorage = TraceStorage(introspy_db_filename)
+        self.analyzer = Analyzer(introspy_db_filename, signature_list)
         
         
     def write_to_directory(self, directory):
         
         # Copy the template
         shutil.copytree('./html', directory)
+        
+        # Copy the DB file
+        shutil.copy(self.db_filename, directory)
         
         # Dump the traced calls
         self.traceStorage.write_to_JS_file(directory)
