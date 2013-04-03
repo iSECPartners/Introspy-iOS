c8c4d559c9fb83795cee22f28ade1887d654ced4
thirstscolr <thirstscolr@mitchbreathely.org>
2013-04-02 19:04:24 -0700

    converting tabs to 4 spaces

--- analyzer/Analysis.py
+++ analyzer/Analysis.py
@@ -7,45 +7,45 @@ from Signatures import Signature
 class Analyzer:
 	""" Manages signature loading and matching """
 
-	def __init__(self, introspy_db_path, signatures, group=None, subgroup=None, list_only=False):
-		self.tracedCalls = self.fetch_and_filter_calls(introspy_db_path, group, subgroup)
-		self.signatures = signatures
-		self.findings = []
-		# List all calls (optionally filtered by group/subgroup) and bypass signature analysis
-		if list_only:
-			self.findings.append(("Call List", self.tracedCalls))
-		# Try each signature on the list of traced calls
-		else:
-		    for sig in self.signatures:
-			self.findings.append((sig, sig.analyze_trace(self.tracedCalls)))
+        def __init__(self, introspy_db_path, signatures, group=None, subgroup=None, list_only=False):
+            self.tracedCalls = self.fetch_and_filter_calls(introspy_db_path, group, subgroup)
+            self.signatures = signatures
+            self.findings = []
+            # List all calls (optionally filtered by group/subgroup) and bypass signature analysis
+            if list_only:
+            	self.findings.append(("Call List", self.tracedCalls))
+            # Try each signature on the list of traced calls
+            else:
+                for sig in self.signatures:
+            	self.findings.append((sig, sig.analyze_trace(self.tracedCalls)))
 
-	def get_findings(self):
-		return self.findings
+        def get_findings(self):
+            return self.findings
 
 	def fetch_and_filter_calls(self, introspy_db_path, group=None, subgroup=None):
-		# the db is on device so we need to grab a local copy
-		if introspy_db_path == 'remote':
-		  scp = ScpClient()
-		  introspy_db_path = scp.select_and_fetch_db()
-		return TraceStorage(introspy_db_path).get_traced_calls(group, subgroup)
+            # the db is on device so we need to grab a local copy
+            if introspy_db_path == 'remote':
+              scp = ScpClient()
+              introspy_db_path = scp.select_and_fetch_db()
+            return TraceStorage(introspy_db_path).get_traced_calls(group, subgroup)
 
-	def write_to_JS_file(self, fileDir, fileName='findings.js'):
-		# Convert the list of findings to a JS var declaration
-		findings_dict = {}
-		findings_dict['findings'] = []
-		for (sig, tracedCalls) in self.findings:
-			if tracedCalls:
-				findings_dict['findings'].append({
-					'signature' : sig, 'calls' : tracedCalls})
-		try:
-			findings_json = json.dumps(findings_dict, cls=TracedCallJSONEncoder)
-		except TypeError as e:
-			print e
-			raise
-		JS_data = 'var findings = ' + findings_json + ';'
-
-		# Write the result to a file
-		JS_filePath = os.path.join(fileDir, fileName)
-		JS_file = open(JS_filePath, 'w')
-		JS_file.write(JS_data)
-
+        def write_to_JS_file(self, fileDir, fileName='findings.js'):
+            # Convert the list of findings to a JS var declaration
+            findings_dict = {}
+            findings_dict['findings'] = []
+            for (sig, tracedCalls) in self.findings:
+                if tracedCalls:
+                    findings_dict['findings'].append({'signature' : sig,
+			    'calls' : tracedCalls})
+            try:
+                findings_json = json.dumps(findings_dict, cls=TracedCallJSONEncoder)
+            except TypeError as e:
+                print e
+                raise
+            JS_data = 'var findings = ' + findings_json + ';'
+            
+            # Write the result to a file
+            JS_filePath = os.path.join(fileDir, fileName)
+            JS_file = open(JS_filePath, 'w')
+            JS_file.write(JS_data)
+            
--- analyzer/introspy.py
+++ analyzer/introspy.py
@@ -9,44 +9,43 @@ from Signatures import signature_list
 from HTMLReport import HTMLReport
 from APIGroups import APIGroups
 
-__author__    = "Tom Daniels & Alban Diquet"
-__license__    = "See ../LICENSE"
-__copyright__    = "Copyright 2013, iSEC Partners, Inc."
+__author__   = "Tom Daniels & Alban Diquet"
+__license__  = "See ../LICENSE"
+__copyright_ = "Copyright 2013, iSEC Partners, Inc."
 
 def main(argv):
-	parser = ArgumentParser(description="introspy analysis tool")
-	parser.add_argument("db",
-			help="Tthe introspy-generated database to analyze.\
-			specifying 'remote' initiates the analyzer to fetch a\
-			remote database from an iOS device.")
-	parser.add_argument("-o", "--outdir",
-			help="Generate an HTML report and write it to the\
-			specified directory (ignores all other command line\
-			optons).")
-	parser.add_argument("-g", "--group",
-			choices=APIGroups.API_GROUPS_LIST,
-			help="Filter by signature group")
-	parser.add_argument("-s", "--sub-group",
-			choices=APIGroups.API_SUBGROUPS_LIST,
-			help="Filter by signature sub-group")
-	parser.add_argument("-l", "--list",
-			action="store_true",
-			help="List traced calls (no signature analysis\
-			performed)")
-	args = parser.parse_args()
-
-	if args.outdir:
-		report = HTMLReport(args.db, signature_list)
-		report.write_to_directory(args.outdir)
-	else:
-		analyzer = Analyzer(args.db, signature_list, args.group, args.sub_group, args.list)
-		for (signature, matching_calls) in analyzer.get_findings():
-			# Hide empty results
-			# TODO: Print other stuff ? signature name or group ?
-			if matching_calls:
-				print "# %s" % signature if isinstance(signature, str) else signature.description
-				for traced_call in matching_calls:
-					print "  %s" % traced_call
+    parser = ArgumentParser(description="introspy analysis tool")
+    parser.add_argument("db",
+        help="Tthe introspy-generated database to analyze.\
+        specifying 'remote' initiates the analyzer to fetch a\
+        remote database from an iOS device.")
+    parser.add_argument("-o", "--outdir",
+        help="Generate an HTML report and write it to the\
+        specified directory (ignores all other command line\
+        optons).")
+    parser.add_argument("-g", "--group",
+        choices=APIGroups.API_GROUPS_LIST,
+        help="Filter by signature group")
+    parser.add_argument("-s", "--sub-group",
+        choices=APIGroups.API_SUBGROUPS_LIST,
+        help="Filter by signature sub-group")
+    parser.add_argument("-l", "--list",
+        action="store_true",
+        help="List traced calls (no signature analysis\
+        performed)")
+    args = parser.parse_args()
+    
+    if args.outdir:
+        report = HTMLReport(args.db, signature_list)
+        report.write_to_directory(args.outdir)
+    else:
+        analyzer = Analyzer(args.db, signature_list, args.group, args.sub_group, args.list)
+        for (signature, matching_calls) in analyzer.get_findings():
+            # TODO: Print other stuff ? signature name or group ?
+            if matching_calls:
+                print "# %s" % signature if isinstance(signature, str) else signature.description
+                for traced_call in matching_calls:
+                    print "  %s" % traced_call
 
 if __name__ == "__main__":
     main(argv[1:])
