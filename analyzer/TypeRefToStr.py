07e4444c5bea4dca9b83ef617c964a20fdc9ac52
thirstscolr <thirstscolr@mitchbreathely.org>
2013-08-13 18:06:31 -0700

    more typrefs to strings

--- analyzer/TypeRefToStr.py
+++ analyzer/TypeRefToStr.py
@@ -1,19 +1,36 @@
 from plistlib import Data
 
 enum_list = {
-    # kSecAttrAccessible values
+    # Keychain
+    ## kSecAttrAccessible values
     'pdmn' : {'ak' : 'kSecAttrAccessibleWhenUnlocked',
               'aku': 'kSecAttrAccessibleWhenUnlockedThisDeviceOnly',
               'dk' : 'kSecAttrAccessibleAlways',
               'dku': 'kSecAttrAccessibleAlwaysThisDeviceOnly',
               'ck' : 'kSecAttrAccessibleAfterFirstUnlock',
               'cku': 'kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly'},
-    # NSDataWritingOptions
+    # NSData
+    ## NSDataWritingOptions
     'mask' : {0x10000000 : 'NSDataWritingFileProtectionNone',
               0x20000000 : 'NSDataWritingFileProtectionComplete',
               0x30000000 : 'NSDataWritingFileProtectionCompleteUnlessOpen',
               0x40000000 : 'NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication',
               'mask'     : 0xf0000000},
+    # CCCrypt and friends
+    ## algorithm
+    'alg'  : {0 : 'kCCAlgorithmAES128',
+              1 : 'kCCAlgorithmDES',
+              2 : 'kCCAlgorithm3DES',
+              3 : 'kCCAlgorithmCAST',
+              4 : 'kCCAlgorithmRC4',
+              5 : 'kCCAlgorithmRC2',
+              6 : 'kCCAlgorithmBlowfish'},
+    ## operation
+    'op'   : {0 : 'kCCEncrypt',
+	      1 : 'kCCDecrypt'},
+    ## options
+    'options' : {0x001 : 'kCCOptionPKCS7Padding',
+	         0x002 : 'kCCOptionECBMode'},
 }
 
 class TypeRefToStr:
@@ -34,11 +51,9 @@ class TypeRefToStr:
                 if v[0] in enum_list:
                     try:
                         if 'mask' in enum_list[v[0]]:
-		            print "FOUND MASK {0} => {1}".format(v[0], v[1])
-                            print "REPLACING WITH {0} => {1}".format(v[0], enum_list[v[0]][v[1] & enum_list[v[0]]['mask']])
+                            print "{0} => {1}".format(v[0], enum_list[v[0]][v[1] & enum_list[v[0]]['mask']])
 			    d[v[0]] = enum_list[v[0]][v[1] & enum_list[v[0]]['mask']]
                         else:
-		            print "{0} => {1}".format(v[0], v[1])
                             print "{0} => {1}".format(v[0], enum_list[v[0]][v[1]])
                             d[v[0]] = enum_list[v[0]][v[1]]
                     except KeyError:
